47a41079b86f1a99fb979b71416f71ec65f007cb
kiril <kiril@LAPTOP-T1KK7D8U>
2022-05-21 11:00:51 +0300

    15 zadanie

--- lab12/pl12.pl
+++ lab12/pl12.pl
@@ -1,4 +1,4 @@
-%найти количество чисел, взаимно простых с заданным
+%11 найти количество чисел, взаимно простых с заданным
 nod(A,0,A):-!.
 nod(A,B,X):-C is A mod B,nod(B,C,X).
 
@@ -12,7 +12,7 @@ eulerU(_,0,0):-!.
 eulerU(A,X,R):-R1 is R-1,nod(A,R,NOD),NOD is 1,eulerU(A,X1,R1),X is X1+1,!.
 eulerU(A,X,R):-R1 is R-1,eulerU(A,X1,R1),X is X1.
 
-% Найти делитель числа, являющийся взаимно простым с наибольшим
+%12 Найти делитель числа, являющийся взаимно простым с наибольшим
 % количество цифр данного числа.
 
 delNum(A,X):- delNum(X,A,A,0,0).
@@ -50,7 +50,29 @@ write_list([Head|Tail]):-write(Head), write(" "), write_list(Tail).
 
 read_list(0, []):-!.
 read_list(N, [X|T]):- read(X), N1 is N-1, read_list(N1, T).
-%длина списка
+%14 длина списка
+
 list_length([H|T], Length):-list_length([H|T], 0, Length).
 list_length([], Length, Length):-!.
 list_length([_|T], CurLength, Length):- CurN1 is CurLength+1, list_length(T, CurN1, Length).
+
+%15
+getMaxInd([H|T],MaxInd):-getMaxInd([H|T],H,0,0,MaxInd).
+getMaxInd([],_,Answer,_,Answer):-!.
+getMaxInd([H|T],Max,_,CurInd,Answer):-CurInd1 is CurInd+1,H>Max,NewMaxInd is CurInd,NewMax is H,getMaxInd(T,NewMax,NewMaxInd,CurInd1,Answer),!.
+getMaxInd([_|T],Max,MaxInd,CurInd,Answer):-CurInd1 is CurInd+1,getMaxInd(T,Max,MaxInd,CurInd1,Answer).
+
+task15:-write("Dlina spiska: "), read(Count),readList(Count,List),
+    write("Kolvo elementov posle max: "),
+    list_length(List,Len),getMaxInd(List,IndMax),
+    X is Len-IndMax-1, write(X),!.
+
+
+
+
+
+
+
+
+
+
